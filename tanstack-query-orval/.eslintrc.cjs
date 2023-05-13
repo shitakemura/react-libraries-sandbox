bcbf2d81e219b7f2360cd6ce5e5499ef888e11da
shitakemura <shitakemura@gmail.com>
2023-05-13 10:17:03 +0900

    install @tanstack/react-query and orval

--- tanstack-query-orval/.eslintrc.cjs
+++ tanstack-query-orval/.eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
--- tanstack-query-orval/README.md
+++ tanstack-query-orval/README.md
@@ -1,1 +1,10 @@
 ## tanstack-query-orval
+
+#### libraries
+
+- TanStack Query: https://tanstack.com/query/latest/docs/react/installation
+- Oval: https://orval.dev/installation
+
+#### Swagger Viewer
+
+- Shift + Option + P
