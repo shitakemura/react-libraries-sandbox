c1282c17ba7307fad761eb3a69d1003cf4f9cca7
shitakemura <shitakemura@gmail.com>
2023-05-23 10:17:31 +0900

    Create project

---
+++ redux-toolkit/redux-toolkit-quick-starts/README.md
@@ -0,0 +1,4 @@
+## Redux Toolkit TypeScript Quick Start
+
+https://redux-toolkit.js.org/tutorials/typescript
+
--- redux-toolkit/redux-toolkit-quick-starts/src/App.tsx
+++ redux-toolkit/redux-toolkit-quick-starts/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 function App() {
-  return <>Redux Toolkit</>
+  return <>Redux Toolkit TypeScript Quick Start</>
 }
 
 export default App
