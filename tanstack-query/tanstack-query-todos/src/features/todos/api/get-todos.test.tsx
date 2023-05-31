ca6220006c23a1aec23725ad3cca09c86ef39615
shitakemura <shitakemura@gmail.com>
2023-06-01 07:20:46 +0900

    Move react query provider for test to util

--- tanstack-query/tanstack-query-todos/src/features/todos/api/get-todos.test.tsx
+++ tanstack-query/tanstack-query-todos/src/features/todos/api/get-todos.test.tsx
@@ -1,24 +1,8 @@
-import { ReactNode } from 'react'
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { renderHook } from '@testing-library/react'
 import { waitFor } from '@testing-library/react'
 
 import { useTodos } from './get-todos'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-    },
-  },
-})
-
-const wrapper = ({ children }: { children: ReactNode }) => {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
-}
+import { renderHook, wrapper } from '@/testing/testing-library-utils'
 
 test('getTodos', async () => {
   const { result } = renderHook(() => useTodos(), { wrapper })
--- tanstack-query/tanstack-query-todos/src/testing/testing-library-utils.tsx
+++ tanstack-query/tanstack-query-todos/src/testing/testing-library-utils.tsx
@@ -1,6 +1,7 @@
 // MEMO: https://testing-library.com/docs/react-testing-library/setup/#custom-render
-import { ReactElement } from 'react'
+import { ReactElement, ReactNode } from 'react'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RenderOptions, render } from '@testing-library/react'
 
 import { AppProvider } from '@/providers/app'
@@ -10,6 +11,22 @@ const customRender = (
   options?: Omit<RenderOptions, 'wapper'>
 ) => render(ui, { wrapper: AppProvider, ...options })
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false, // set false for test
+    },
+  },
+})
+
+// wrapper to use test for React Query hook
+const customWrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
+
 export * from '@testing-library/react'
 
 export { customRender as render }
+export { customWrapper as wrapper }
