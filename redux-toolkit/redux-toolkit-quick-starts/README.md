d027c1c0fe2afa23f1bf7276fa0123c77beeab0b
shitakemura <shitakemura@gmail.com>
2023-06-23 10:05:50 +0900

    Add bookReducer to store

--- redux-toolkit/redux-toolkit-quick-starts/README.md
+++ redux-toolkit/redux-toolkit-quick-starts/README.md
@@ -2,6 +2,7 @@
 
 - https://redux-toolkit.js.org/tutorials/typescript
 - https://redux-toolkit.js.org/tutorials/quick-start
+- https://ogzhanolguncu.com/blog/react-redux-toolkit-with-typescript
 
 #### memo
 
---
+++ redux-toolkit/redux-toolkit-quick-starts/src/features/book/bookSlice.ts
@@ -0,0 +1,62 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../app/store'
+import { v4 as uuidv4 } from 'uuid'
+
+type Book = {
+  id: string
+  title: string
+  author?: string
+}
+
+type BookState = {
+  bookList: Book[]
+}
+
+const initialState: BookState = {
+  bookList: [
+    {
+      id: uuidv4(),
+      title: '1984',
+      author: 'George Orwell',
+    },
+    {
+      id: uuidv4(),
+      title: "Harry Potter and the Philosopher's Stone",
+      author: 'J. K. Rowling',
+    },
+    {
+      id: uuidv4(),
+      title: 'The Lord of the Rings',
+      author: 'J.R.R Tolkien',
+    },
+  ],
+}
+
+export const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    addNewBook: (state, action: PayloadAction<Book>) => {
+      state.bookList.push(action.payload)
+    },
+    updateBook: (state, action: PayloadAction<Book>) => {
+      const {
+        payload: { id, title, author },
+      } = action
+
+      state.bookList = state.bookList.map((book) => {
+        return book.id === id ? { ...book, title, author } : book
+      })
+    },
+    deleteBook: (state, action: PayloadAction<{ id: string }>) => {
+      state.bookList = state.bookList.filter(
+        (book) => book.id !== action.payload.id
+      )
+    },
+  },
+})
+
+export const { addNewBook, updateBook, deleteBook } = bookSlice.actions
+
+export const selectBookList = (state: RootState) => state.book.bookList
+export default bookSlice.reducer
