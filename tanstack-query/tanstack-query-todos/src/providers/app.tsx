be84aeeb5a401b9a3b01f9653d04ed23c9b6d962
shitakemura <shitakemura@gmail.com>
2023-07-16 14:09:47 +0900

    Install react query devtools

--- tanstack-query/tanstack-query-todos/src/providers/app.tsx
+++ tanstack-query/tanstack-query-todos/src/providers/app.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { queryClient } from '@/lib/react-query'
 
@@ -18,6 +19,7 @@ export const AppProvider = ({ children }: AppProviderProps) => {
       >
         {children}
       </ErrorBoundary>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
